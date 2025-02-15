565565ea3298fd1a3185815b50c0b6b6d3fc6b80
minnong <ongminn@gmail.com>
2025-02-15 20:23:17 +0800

    - Added Clerk login and registration pages - To change UI

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,33 +7,55 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import * as SecureStore from 'expo-secure-store';
+import { ClerkProvider, useAuth } from '@clerk/clerk-expo';
+import { Slot, useRouter, useSegments } from 'expo-router';
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+const tokenCache = {
+	async getToken(key: string) {
+		try {
+			return SecureStore.getItemAsync(key);
+		} catch (err) {
+			return null;
+		}
+	},
+	async saveToken(key: string, value: string) {
+		try {
+			return SecureStore.setItemAsync(key, value);
+		} catch (err) {
+			return;
+		}
+	}
+};
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
+const InitialLayout = () => {
+	const { isLoaded, isSignedIn } = useAuth();
+	const segments = useSegments();
+	const router = useRouter();
 
-  if (!loaded) {
-    return null;
-  }
+	useEffect(() => {
+		if (!isLoaded) return;
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
-}
+		const inTabsGroup = segments[0] === '(auth)';
+
+		if (isSignedIn && !inTabsGroup) {
+			router.replace('/home');
+		} else if (!isSignedIn) {
+			router.replace('/login');
+		}
+	}, [isSignedIn]);
+
+	return <Slot />;
+};
+
+const RootLayoutNav = () => {
+	return (
+		<ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY!} tokenCache={tokenCache}>
+			<InitialLayout />
+		</ClerkProvider>
+	);
+};
+
+export default RootLayoutNav;
